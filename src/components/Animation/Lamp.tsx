e5cf88a1302232219d6b05c432e9ebd3485993ee
undefined9010 <undefined@gmail.com>
2025-04-08 20:12:10 +0200

    feat(loader): remove preloader, tried to fix lamp

--- src/components/Animation/Lamp.tsx
+++ src/components/Animation/Lamp.tsx
@@ -19,11 +19,11 @@ export const LampContainer = ({
     >
       <div className="relative flex w-full flex-1 scale-y-[80%] items-center justify-center isolate z-0 ">
         <motion.div
-          initial={{ opacity: 0.5, width: "15rem" }}
+          initial={{ opacity: 0.1, width: "15rem" }}
           whileInView={{ opacity: 1, width: "30rem" }}
           transition={{
-            delay: 0.3,
-            duration: 0.8,
+            delay: 0.1,
+            duration: 0.5,
             ease: "easeInOut",
           }}
           style={{
@@ -32,11 +32,11 @@ export const LampContainer = ({
           className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-[#07c1b6] via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
         ></motion.div>
         <motion.div
-          initial={{ opacity: 0.5, width: "15rem" }}
+          initial={{ opacity: 0.1, width: "15rem" }}
           whileInView={{ opacity: 1, width: "30rem" }}
           transition={{
-            delay: 0.3,
-            duration: 0.8,
+            delay: 0.1,
+            duration: 0.5,
             ease: "easeInOut",
           }}
           style={{
@@ -51,8 +51,8 @@ export const LampContainer = ({
           initial={{ width: "8rem" }}
           whileInView={{ width: "16rem" }}
           transition={{
-            delay: 0.3,
-            duration: 0.8,
+            delay: 0.1,
+            duration: 0.5,
             ease: "easeInOut",
           }}
           className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-[#07c1b6] blur-2xl"
@@ -61,8 +61,8 @@ export const LampContainer = ({
           initial={{ width: "15rem" }}
           whileInView={{ width: isMobile ? "70%" : "100%" }}
           transition={{
-            delay: 0.3,
-            duration: 0.8,
+            delay: 0.1,
+            duration: 0.5,
             ease: "easeInOut",
           }}
           className="hidden sm:visible absolute inset-auto z-50 h-0.5 -translate-y-[7rem] bg-[#07c1b6] "
--- src/components/Layouts/RegularLayout/index.tsx
+++ src/components/Layouts/RegularLayout/index.tsx
@@ -1,40 +1,15 @@
-import { useState, useEffect } from "react";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Outlet } from "react-router";
-import { AnimatePresence, motion } from "framer-motion";
-import { Preloader } from "@/components/Preloader";
 
 export const RegularLayout = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1500);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <>
-      <AnimatePresence>
-        {isLoading && <Preloader key="layout-preloader" />}
-      </AnimatePresence>
-
-      <motion.div
-        className="flex flex-col min-h-screen"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: isLoading ? 0 : 1 }}
-        transition={{ duration: 0.5, delay: isLoading ? 0 : 0.1 }}
-      >
-        <Header />
-
-        <main className="flex-1 overflow-hidden">
-          <Outlet />
-        </main>
-        <Footer />
-      </motion.div>
-    </>
+    <div className="flex flex-col max-h-screen ">
+      <Header />
+      <main className="flex-1 overflow-hidden">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 };
